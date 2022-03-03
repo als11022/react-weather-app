b7978463c30e0a7daf1fadcdde5ac5b60bac7be8
als11022 <95195221+als11022@users.noreply.github.com>
2022-03-03 15:43:04 -0500

    Added github repo url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"> Weather App </div>
+    <div className="App"> 
+    <h1> Weather App </h1>
+    <footer>This project was coded by 
+      <a href="https://keen-wing-9e9d53.netlify.app/" 
+      title="portfolio for Abby Szach" 
+      target="_blank"> 
+      Abby Szach 
+      </a> and is {" "}
+    <a href="https://github.com/als11022/react-weather-app" 
+    target="_blank"
+    title="github repository">
+      open-sourced on Github
+      </a>
+    </footer>
+    </div>
   );
 }
 
