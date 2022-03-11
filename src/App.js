d1865ca9cc5b794d5f5ff8a962018363c3cebb6a
als11022 <95195221+als11022@users.noreply.github.com>
2022-03-11 16:36:10 -0500

    fixed spacing

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ function App() {
   return (
     <div className="App"> 
     <h1> Weather App </h1>
-    <footer>This project was coded by 
+    <footer>This project was coded by {" "}
       <a href="https://keen-wing-9e9d53.netlify.app/" 
-      title="portfolio for Abby Szach" 
-      target="_blank"> 
-      Abby Szach 
-      </a> and is {" "}
+            title="portfolio for Abby Szach" 
+           target="_blank"> 
+      Abby Szach
+      </a> 
+      {" "} and is {" "}
     <a href="https://github.com/als11022/react-weather-app" 
-    target="_blank"
-    title="github repository">
+         target="_blank"
+         title="github repository">
       open-sourced on Github
       </a>
     </footer>
